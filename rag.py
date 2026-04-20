da1d5b05f797421565194c89e07e8eb321e2bbaf
Wali Mohamed <walimoh@gmail.com>
2026-04-21 00:05:19 +0100

    added streamlit and api key

--- rag.py
+++ rag.py
@@ -5,9 +5,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
 
-Settings.llm = OpenAI(model="gpt-4o", temperature=0.1)
-Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+Settings.llm = OpenAI(model="gpt-4o", temperature=0.1, api_key=api_key)
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small", api_key=api_key)
 
 documents = SimpleDirectoryReader("./history_pages").load_data()
 index = VectorStoreIndex.from_documents(documents)
